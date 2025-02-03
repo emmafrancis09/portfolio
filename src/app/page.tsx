9c6481156e0cda7b39126b3cf9d47bfcdae3d5b4
ProdFilthi <155994707+ProdFilthi@users.noreply.github.com>
2025-02-03 22:08:01 +0200

    added personal details and social links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,35 @@
+import Image from "next/image";
 import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const page = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="flex items-center justify-center flex-col py-4">
+        <Image
+          src={"/Filthii.png"}
+          alt="profile-picture"
+          width={3024}
+          height={4032}
+          className="rounded-full object-cover  w-[200px] h-[200px] shadow shadow-blue-500"
+        />
+        <h1 className="py-4 text-sm flex items-center justify-center flex-col leading-6">
+          <p>
+            My name is{" "}
+            <span className="font-semi-bold text-xl">Emmanuel Francis</span>
+          </p>
+          <p>I am a 24 years old software developer</p>
+          <p>Located in Rwanda 🇷🇼</p>
+        </h1>
+        <div>
+          <div className="flex items-center justify-center gap-2 text-3xl">
+            <FaGithub className="cursor-pointer hover:shadow-blue-600 shadow-md transition-all rounded-full" />
+            <FaLinkedin className="cursor-pointer hover:shadow-blue-600 shadow-md transition-all" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default page;
--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -7,7 +7,7 @@ interface ContainerProps {
 const Container: React.FC<ContainerProps> = ({ children }) => {
   return (
     <div className="relative h-full w-full bg-slate-950 min-h-screen">
-      <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#3e3e3e,transparent)] text-white">
+      <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#3e3e3e,transparent)] text-white font-[poppins]">
         {children}
       </div>
     </div>
